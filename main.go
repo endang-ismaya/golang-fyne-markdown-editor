20195d15888d015b056da977ab926ec2df849936
Endang Ismaya <endang.ismaya@gmail.com>
2022-09-11 14:15:45 +0700

    add savefile dialog function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -28,6 +29,20 @@ func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
 	return editWidget, previewWidget
 }
 
+func (app *config) createMenuItems(win fyne.Window) {
+
+	openMenuItem := fyne.NewMenuItem("Open...", func() {})
+	saveMenuItem := fyne.NewMenuItem("Save", func() {})
+	app.SaveMenuItem = saveMenuItem
+	app.SaveMenuItem.Disabled = true
+	saveAsMenuItem := fyne.NewMenuItem("Save as...", app.saveAsFunc(win))
+	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
+
+	menu := fyne.NewMainMenu(fileMenu)
+
+	win.SetMainMenu(menu)
+}
+
 func main() {
 	// create a fyne app
 	fyneApp := app.New()
@@ -37,6 +52,7 @@ func main() {
 
 	// get the user interface
 	editWidget, previewWidget := cfg.makeUI()
+	cfg.createMenuItems(win)
 
 	// set the content of the window
 	win.SetContent(container.NewHSplit(editWidget, previewWidget))
@@ -46,4 +62,30 @@ func main() {
 	win.CenterOnScreen()
 	win.ShowAndRun()
 
+}
+
+func (app *config) saveAsFunc(win fyne.Window) func() {
+	return func() {
+		saveDialog := dialog.NewFileSave(func(write fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+			}
+
+			if write == nil {
+				// user canceled
+				return
+			}
+
+			// save the file
+			write.Write([]byte(app.EditWidget.Text))
+			app.CurrentFile = write.URI()
+
+			defer write.Close()
+
+			win.SetTitle(win.Title() + " - " + write.URI().Name())
+			app.SaveMenuItem.Disabled = false
+		}, win)
+
+		saveDialog.Show()
+	}
 }
